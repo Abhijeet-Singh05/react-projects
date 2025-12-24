dbbb07b8462dd511a30f6ad958882c4a4508dde8
Abhijeet-Singh05 <singh.abhijeet9342@gmail.com>
2025-12-24 14:21:47 +0530

    custom react

--- 01vite-react/src/App.jsx
+++ 01vite-react/src/App.jsx
@@ -4,9 +4,15 @@
 
 function App() {
   
+  // using variable
+  const username = "ihba"
 
   return (
-    <h1>Vite React</h1>
+    <>
+      <h1>Vite React</h1>
+      <p>made by {username}</p>
+      
+    </>
   )
 }
 
--- 01vite-react/src/main.jsx
+++ 01vite-react/src/main.jsx
@@ -1,9 +1,28 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+// creating object
+
+// const Element = {
+//   <a href="https://google.com" target ="_blank"> Google </a>
+// }
+
+// Right method
+const ReactElement = React.createElement( 
+  "a",{
+    href:"https://google.com",
+    target: "_blank"
+  },
+  "click me "
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ 
+  
+  
+  ReactElement
+  
+  
 )
